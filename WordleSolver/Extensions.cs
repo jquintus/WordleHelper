f2c6a7ee9a9d7966bfcdb99711aa1ec22d7c5c0f
Josh Quintus <josh.quintus@gmail.com>
2023-03-04 16:47:00 -0500

    Logic works
    Styling needs to be redone

---
+++ WordleSolver/Extensions.cs
@@ -0,0 +1,47 @@
+﻿namespace WordleSolver
+{
+	public static class Extentions
+	{
+		public static string JoinToString(this IEnumerable<string> source, string separator = "")
+		{
+			return string.Join(separator, source);
+		}
+
+		public static string JoinToString(this IEnumerable<char> source, string separator = "")
+		{
+			return string.Join(separator, source);
+		}
+
+		public static bool None<TSource>(this IEnumerable<TSource> source)
+		{
+			return !source.Any();
+		}
+
+		public static bool None<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+		{
+			return !source.Any(predicate);
+		}
+
+		public static IEnumerable<TSource> Order<TSource>(this IEnumerable<TSource> source)
+		{
+			return source.OrderBy(x => x);
+		}
+
+		public static IEnumerable<string> Permutate(this string source) =>
+		 source
+			.AsEnumerable()
+			.Permutate()
+			.Select(a => new string(a));
+
+		public static IEnumerable<T[]> Permutate<T>(this IEnumerable<T> source)
+		{
+			return permutate(source, Enumerable.Empty<T>());
+			IEnumerable<T[]> permutate(IEnumerable<T> reminder, IEnumerable<T> prefix) =>
+				!reminder.Any() ? new[] { prefix.ToArray() } :
+				reminder.SelectMany((c, i) => permutate(
+					reminder.Take(i).Concat(reminder.Skip(i + 1)).ToArray(),
+					prefix.Append(c)));
+		}
+	}
+
+}
--- WordleSolver/Models/InputModel.cs
+++ WordleSolver/Models/InputModel.cs
@@ -1,38 +1,103 @@
 ﻿namespace WordleSolver.Models
 {
+	public class GuessModel
+	{
+		private readonly string _guess;
+		public List<LetterModel> Letters { get; }
+
+		public GuessModel(string guess)
+		{
+			_guess = guess;
+			Letters = guess
+				.Select((l, idx) => new LetterModel(l.ToString(), idx, LetterState.NotPresent))
+				.ToList();
+		}
+
+		public override string ToString() => _guess;
+	}
+
 	public class InputModel
 	{
-		public List<JoshModel> Guesses { get; set; }
-		public List<string> Suggestions { get; set; }
+		private readonly Wordlizer _service;
+
+		public List<GuessModel> Guesses { get; private set; }
+		public List<string> Suggestions { get; private set; }
 		public string Word { get; set; }
 
-		public InputModel(object obj)
+		public InputModel(Wordlizer service)
 		{
-			Guesses = new List<JoshModel>();
+			_service = service;
+			Word = string.Empty;
+			Guesses = new List<GuessModel>();
 			Suggestions = new List<string>();
 		}
 
-		public void AddGuess(string word)
+		public void AddGuess(string guess)
 		{
+			if (string.IsNullOrEmpty(guess)) return;
+			Guesses.Add(new(guess.ToUpper()));
 		}
 
 		public void GenerateSuggestions()
-		{ }
+		{
+			var letters = Guesses
+				.SelectMany(g => g.Letters)
+				.Select(l => new WordleLetter(l.Letter.First(), l.Position, l.State));
+
+			Suggestions = _service
+				.GeneratePermutations(letters)
+				.ToList();
+		}
 
 		public void Reset()
-		{ }
+		{
+			Guesses = new List<GuessModel>();
+			Suggestions = new List<string>();
+		}
 	}
 
-	public class JoshModel
+
+	public class LetterModel
 	{
-		public List<LetterModel> Letters { get; set; } = new List<LetterModel>();
-	}
-	public class LetterModel { 
+		public string Color => State switch
+		{
+			LetterState.NotPresent => "#787c7e",
+			LetterState.WrongPosition => "#c9b458",
+			LetterState.CorrectPosition => "#6aaa64",
+			_ => "#787c7e",
+		};
+
+		public string Letter { get; }
+
+		public int Position { get; }
+
+		public LetterState State { get; private set; }
+
+		public LetterModel(string letter, int position, LetterState state = LetterState.NotPresent)
+		{
+			Letter = letter;
+			Position = position;
+			State = state;
+		}
+
 		public void ToggleState()
 		{
+			State = State switch
+			{
+				LetterState.NotPresent => LetterState.WrongPosition,
+				LetterState.WrongPosition => LetterState.CorrectPosition,
+				LetterState.CorrectPosition => LetterState.NotPresent,
 
+				_ => LetterState.NotPresent,
+			};
 		}
-		public string Color { get; set; } = "red";
 
-    }
+		public override string ToString() => Letter;
+	}
+	public enum LetterState
+	{
+		NotPresent,
+		WrongPosition,
+		CorrectPosition,
+	}
 }
