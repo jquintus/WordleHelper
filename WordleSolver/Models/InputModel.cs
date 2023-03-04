cdac5684edbd965a52c5b7e7d9d45d92afc1c5fc
Josh Quintus <josh.quintus@gmail.com>
2023-03-04 16:39:47 -0500

    Stubbed out enough so that the UI can be shown
    It doesn't work yet though.

---
+++ WordleSolver/Models/InputModel.cs
@@ -0,0 +1,38 @@
+﻿namespace WordleSolver.Models
+{
+	public class InputModel
+	{
+		public List<JoshModel> Guesses { get; set; }
+		public List<string> Suggestions { get; set; }
+		public string Word { get; set; }
+
+		public InputModel(object obj)
+		{
+			Guesses = new List<JoshModel>();
+			Suggestions = new List<string>();
+		}
+
+		public void AddGuess(string word)
+		{
+		}
+
+		public void GenerateSuggestions()
+		{ }
+
+		public void Reset()
+		{ }
+	}
+
+	public class JoshModel
+	{
+		public List<LetterModel> Letters { get; set; } = new List<LetterModel>();
+	}
+	public class LetterModel { 
+		public void ToggleState()
+		{
+
+		}
+		public string Color { get; set; } = "red";
+
+    }
+}
